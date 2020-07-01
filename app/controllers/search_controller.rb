e31458ccb5950fcca687f45891bfa42d402b9932
Ethan Hocking <eshocking253@gmail.com>
2020-07-01 10:20:58 -0600

    Add api request to search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,14 @@
 class SearchController < ApplicationController
   def index
-  end
+    state = params[:state]
 
-  def show
+    conn = Faraday.new(url: "https://api.propublica.org") do |faraday|
+      faraday.headers["X-API-KEY"] = ENV["PROPUBLICA_KEY"]
+    end
+
+    response = conn.get("/congress/v1/members/house/#{state}/current.json")
+
+    json = JSON.parse(response.body, symbolize_names: true)
+    @members = json[:results]
   end
 end
