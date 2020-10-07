8e21e13bcd5fa00c5dd41a7278b653d7f6552756
Dione Wilson <dionew1@gmail.com>
2020-10-06 18:49:51 -0600

    Add feature test

---
+++ spec/features/user_can_search_by_state_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+describe 'user can search for house members' do
+  it 'user submits valid state name' do
+    visit '/'
+
+    select "Colorado", from: :state
+    click_on "Locate Members of the House"
+
+    expect(current_path).to eq(search_path)
+    expect(page).to have_content("7 Results")
+    expect(page).to have_css(".member", count: 7)
+
+    within(first(".member")) do
+      expect(page).to have_css(".name")
+      name = find(".name").text
+      expect(name).to_not be_empty
+
+      expect(page).to have_css(".role")
+      role = find(".role").text
+      expect(role).to_not be_empty
+
+      expect(page).to have_css(".party")
+      party = find(".party").text
+      expect(party).to_not be_empty
+
+      expect(page).to have_css(".district")
+      district = find(".district").text
+      expect(district).to_not be_empty
+    end
+  end
+end
