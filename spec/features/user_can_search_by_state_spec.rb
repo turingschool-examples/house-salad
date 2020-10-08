2c7ec3b9543475ad8cf3e99cbbe7bbc685fee7d9
ian douglas <ian.douglas@iandouglas.com>
2020-10-07 21:33:59 -0600

    added some css

--- spec/features/user_can_search_by_state_spec.rb
+++ spec/features/user_can_search_by_state_spec.rb
@@ -1,31 +1,31 @@
 require 'rails_helper'
 
-describe 'user can search for house members' do
+RSpec.describe 'user can search for house members' do
   it 'user submits valid state name' do
-    visit '/'
+    visit root_path
 
-    select "Colorado", from: :state
-    click_on "Locate Members of the House"
+    select 'Colorado', from: :state
+    click_button 'Locate Members of the House'
 
     expect(current_path).to eq(search_path)
-    expect(page).to have_content("7 Results")
-    expect(page).to have_css(".member", count: 7)
+    expect(page).to have_content('7 Results')
+    expect(page).to have_css('.member', count: 7)
 
-    within(first(".member")) do
-      expect(page).to have_css(".name")
-      name = find(".name").text
+    within(first('.member')) do
+      expect(page).to have_css('.name')
+      name = find('.name').text
       expect(name).to_not be_empty
 
-      expect(page).to have_css(".role")
-      role = find(".role").text
+      expect(page).to have_css('.role')
+      role = find('.role').text
       expect(role).to_not be_empty
 
-      expect(page).to have_css(".party")
-      party = find(".party").text
+      expect(page).to have_css('.party')
+      party = find('.party').text
       expect(party).to_not be_empty
 
-      expect(page).to have_css(".district")
-      district = find(".district").text
+      expect(page).to have_css('.district')
+      district = find('.district').text
       expect(district).to_not be_empty
     end
   end
